f315596441c39d02295c61ecfb47404575a264ae
Mike <michaelokolo62@yahoo.com>
2025-02-17 22:52:51 +0000

    commenced the login form element

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,3 +1,39 @@
+import styles from '@/app/ui/form.module.css';
+
 export default function Page() {
-  return <p>Login</p>;
+  return (
+    <div className={styles.authContainer}>
+      <div className={styles.logoContainer}>
+        <img alt="company logo" src="/logo.png" className={styles.logo} />
+      </div>
+
+      <h2 className={styles.formHeader}>Login</h2>
+      <p>To stay connected with us</p>
+      <div>
+        <form action="#" method="POST" className={styles.formWrapper}>
+          <div>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              autoComplete="email"
+            />
+          </div>
+          <div>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              required
+              autoComplete="current-password"
+            />
+          </div>
+          <div className={styles.btnContainer}>
+            <button className={styles.btn} type="submit">LOG IN</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
